775ac21e7c6036795e274913cffd6ec8124ec130
eduEsron <141090507+eduEsron@users.noreply.github.com>
2024-02-29 17:57:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let a = document.getElementById("ervinha")
+let b = document.getElementById("funguinho")
+let c = document.getElementById("trombeteira")
+let d = document.getElementById("Ayahuasca")
+let e = document.getElementById("Iboga")
+let f = document.getElementById("ParreiraBrava")
+let im = document.getElementById("natuvel")
+a.addEventListener("click",function(){
+    im.src = "./img]/FBI_headpic-1024x576.jpg"
+
+})
+b.addEventListener("click",function(){
+    im.src = "./img]/FBI_headpic-1024x576.jpg"
+})
+c.addEventListener("click",function(){
+    im.src = "./img]/FBI_headpic-1024x576.jpg"
+})
+d.addEventListener("click",function(){
+    im.src = "./img]/FBI_headpic-1024x576.jpg"
+})
+e.addEventListener("click",function(){
+    im.src = "./img]/FBI_headpic-1024x576.jpg"
+})
+f.addEventListener("click",function(){
+    im.src = "./img]/FBI_headpic-1024x576.jpg"
+})
